d17cfb97d90fca2de1a378bfe4061072ae95493e
utku-guclu <utku_guclu@hotmail.com>
2025-02-03 20:43:07 +0300

    prisma setup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,9 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { PrismaModule } from "src/prisma/prisma.module";
 
- @Module({
-    imports: [PrismaModule],
-   controllers: [AuthController],
-   providers: [AuthService],
- })
- export class AuthModule {}
+@Module({
+  imports: [PrismaModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule { }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient{
+export class PrismaService extends PrismaClient {
     constructor() {
         super({
             datasources: {
